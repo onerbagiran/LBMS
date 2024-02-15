4e91e7cbf96768d0461d475db7c7ccb873fe9c9e
onerbagiran <onerbagiran1999@gmail.com>
2024-02-15 17:57:19 +0300

    önerbabba

--- kutuphane.py
+++ kutuphane.py
@@ -1,30 +1,4 @@
-# class Library:
-#     def __init__(self):
-#         self.file=open("books.txt","a+")
-        
-#     def kitap_listele(self):
-#         self.file.seek(0)
-#         books=self.file.read().splitlines()
-#         if not books:
-#             print("Henüz girilmiş kitap yok")
-#         else:
-#                 for book in books:
-#                     Kitap_adi,Yazar_adi,Tarih,Sayfa_sayisi=book.split(",")
-#                     print(f"Kitap Adi: {Kitap_adi}, Yazar : {Yazar_adi}, Çıkış Tarihi: {Tarih}, Sayfa Sayısı: {Sayfa_sayisi}")
-                    
-#     def kitap_ekle(self):
-#         Kitap_adi=input("Kitap adını giriniz:")
-#         Yazar_adi=input(Kitap_adi+"kitabının yazar adını giriniz:")
-#         Tarih=input(Kitap_adi+"kitabının çıkış tarihini giriniz:")
-#         Sayfa_sayisi=input(Kitap_adi + "kitabının sayfa sayisini giriniz:") 
-#         Kitabin_kunyesi=f"{Kitap_adi},{Yazar_adi},{Tarih},{Sayfa_sayisi}\n"
-#         self.file.write(Kitabin_kunyesi)
-#         print("Kitap bilgileri yüklendi")
 
-
-#     def kitap_silme(self):
-#         Kitap_adi_remove=input("Silmek istediğiniz kitap adını giriniz:")
-        
              
                 
 class Library:
@@ -34,6 +8,7 @@ class Library:
         self.kitap_ekle()
         self.kitap_listele()
         self.kitap_silme()
+
     def kitap_listele(self):
         self.file.seek(0)
         books=self.file.read().splitlines()
